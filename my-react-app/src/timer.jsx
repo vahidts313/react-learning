984d007c33d995507bdde7ffbe5c9162fd84deeb
vahid <vahidts313@gmail.com>
2026-08-13 15:26:22 +0330

    part 11 +

--- my-react-app/src/timer.jsx
+++ my-react-app/src/timer.jsx
@@ -1,15 +1,33 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
 import "./style.css";
+
+var interval;
 class Timer extends React.Component {
   constructor() {
     super();
-    this.state = { time: new Date().toLocaleTimeString() };
+    this.state = { time: 10 };
   }
-  render() {
-    setInterval(() => {
-      this.setState({ time: new Date().toLocaleTimeString() });
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    interval = setInterval(() => {
+      this.setState({ time: this.state.time - 1 });
     }, 1000);
+  }
+
+  componentDidUpdate() {
+    if (this.state.time === 0) {
+      clearInterval(interval);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(interval);
+  }
+
+  render() {
+    console.log("render");
     return <h2 className="timer">it is {this.state.time}</h2>;
   }
 }
